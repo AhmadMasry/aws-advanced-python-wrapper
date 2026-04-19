e34764a2d444adbf5e4e5e6d4a4254cc195ef810
AhmadMS1988 <masry@harri.com>
2026-04-19 21:14:03 +0300

    feat: add aws_advanced_python_wrapper.psycopg DBAPI submodule

--- tests/unit/test_dbapi_module_contract.py
+++ tests/unit/test_dbapi_module_contract.py
@@ -98,6 +98,7 @@ def test_exception_hierarchy_roots():
 
 @pytest.mark.parametrize("module_name", [
     "aws_advanced_python_wrapper",
+    "aws_advanced_python_wrapper.psycopg",
 ])
 @pytest.mark.parametrize("attr_name", PEP249_NAMES + ("connect",))
 def test_module_exports_pep249_attr(module_name, attr_name):
